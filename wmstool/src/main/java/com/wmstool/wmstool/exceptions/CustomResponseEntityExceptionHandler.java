31f2e839d5ae2f0e1a5de3ba05b5a40c423bf6f8
WeiChih Chen <weichihchen988@icloud.com>
2019-09-28 03:46:17 +0800

    re-define exception handle during inStock process

--- wmstool/src/main/java/com/wmstool/wmstool/exceptions/CustomResponseEntityExceptionHandler.java
+++ wmstool/src/main/java/com/wmstool/wmstool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.wmstool.wmstool.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +15,30 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+	
+	//Deprecated due to Controller annotation @Validated
 	@ExceptionHandler
-	public final ResponseEntity<Object> handleClothIdentifierException(ClothIdentifierException ex,
-			WebRequest request) {
-		ClothIdentifierExceptionResponse exceptionResponse = new ClothIdentifierExceptionResponse(ex.getMessage());
+	public final ResponseEntity<Object> handleInStockRequestException(InStockRequestException ex, WebRequest request) {
+		InStockRequestExceptionResponse exceptionResponse = new InStockRequestExceptionResponse(ex.getMessage());
+		System.out.println(ex.getMessage());
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex,
+			WebRequest request) {
+
+		Map<String, String> errorMap = new HashMap<>();
+		String[] firstSplit = ex.getMessage().split(",");
+
+		for (String s : firstSplit) {
+			String[] secondSplit = s.split(": ");
+
+			errorMap.put(secondSplit[0].split("\\.", 2)[1], secondSplit[1]);
+		}
+
+		return new ResponseEntity<Object>(errorMap, HttpStatus.BAD_REQUEST);
+
 	}
 
 }
